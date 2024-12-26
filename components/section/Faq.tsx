fc37ac610a9d4c3c1320b216ee5ac2bc79127b50
mario <khantkokozaw382000@gmail.com>
2024-12-26 15:18:12 +0800

    fix for vercel

--- components/section/Faq.tsx
+++ components/section/Faq.tsx
@@ -13,10 +13,10 @@ const Faq = () => {
         <div className="container relative z-2 py-28">
           <div>
             <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4">
-              Curiosity didn't kill the cat, it gave it answers.
+              Curiosity didn&apos;t kill the cat, it gave it answers.
             </h3>
             <p className="body-1 max-lg:max-w-sm">
-              You've got questions, we've got answers.
+              You&apos;ve got questions, we&apos;ve got answers.
             </p>
           </div>
 
